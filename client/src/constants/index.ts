184762b17bc8ef57fd5c7241d28b7a788117fb4b
omega <duneomega@gmail.com>
2023-04-04 12:23:34 +0800

    11111

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,10 +6,11 @@ const test = {
   wrapperCol: { flex: 1 },
   autoFocusFirstInput: true,
   preserve: false
+  size: 'large'
 };
 
 const table = {
-    search: 24 12 8 6 6 4
+    search: {span: 24 12 8 6 6 4}
     pagination: [defaultpageSize: 10, showSizeChanger true]
     
 }
